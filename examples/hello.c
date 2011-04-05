586cde81bea61b9fc8479cb128074fb729335a20
Ben Noordhuis <info@bnoordhuis.nl>
2011-04-05 02:17:36 +0200

    Update the example code, make it more idiomatic.

--- examples/hello.c
+++ examples/hello.c
@@ -1,7 +1,7 @@
 #include "http_protocol.h"
 
 int handle_request(request_rec *r) {
-	apr_table_set(r->headers_out, "Content-Type", "text/html; charset=UTF-8");
+	ap_set_content_type(r, "text/html; charset=UTF-8");
 	ap_rputs("Hello, modlet world!", r);
 	return OK;
 }
